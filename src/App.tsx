137dc73836d7d81a019933960a16fe2dd43c1f98
savioramon <savioramon12@gmail.com>
2023-02-27 10:03:44 -0300

    creating and configuring project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./app.style";
+import { GlobalStyle } from "./globalStyle";
+
+
+export default function App() {
+	return (
+		<Container>
+			<GlobalStyle />
+			
+			To-do List
+		</Container>
+	)
+}
