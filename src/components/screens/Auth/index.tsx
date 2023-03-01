20b22d70fdd32b3efde9eac004fcc4a009274fc2
savioramon <savioramon12@gmail.com>
2023-03-01 20:00:59 -0300

    creating auth redirect when route is not specified

--- src/components/screens/Auth/index.tsx
+++ src/components/screens/Auth/index.tsx
@@ -1,7 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../utils/CONSTANTS";
 import { Container } from "./style";
 
 export default function Auth() {
+
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const { LOGIN, REGISTER } = ROUTES;
+        const location = window.location;
+        const path = location.pathname;
+
+        if(!path.includes(LOGIN) && !path.includes(REGISTER)) {
+            navigate(`${LOGIN}`);
+        }
+    }, []);
+
     return (
         <Container>
             <Outlet />
