4b7204e8165c17410bf6183741b037d610f8b893
savioramon <savioramon12@gmail.com>
2023-03-02 22:06:23 -0300

    add changes

--- src/components/screens/Auth/components/LoginComponent/index.tsx
+++ src/components/screens/Auth/components/LoginComponent/index.tsx
@@ -1,11 +1,50 @@
 
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../../../utils/CONSTANTS";
+import Button from "../Button";
+import ButtonInline from "../ButtonInline";
 import Form from "../Form";
+import Input from "../Input";
+import TopContent from "../TopContent";
 
 
 export default function LoginComponent() {
+    const navigate = useNavigate();
+    const { REGISTER, AUTH } = ROUTES;
+
+    function redirectUser(route: string) {
+        navigate(`/${AUTH}/${route}`);
+    }
+
     return (
-        <Form>
-            Login
-        </Form>
+        <Fragment>
+            <TopContent>
+                LOGIN
+            </TopContent>
+            <Form>
+                <Fragment>
+                    <Input type="email" placeholder="put your email..." title="Email" />
+
+                    <Input type="password" placeholder="put your password..." title="Password" />
+
+                    <ButtonInline text="" action={() => {console.log("test")}}>
+                        Forgot your password?
+                    </ButtonInline>
+
+                    <Button>
+                        LOGIN
+                    </Button>
+
+                    <ButtonInline
+                        text="Need an account?"
+                        center={true}
+                        action={() => redirectUser(REGISTER)}
+                    >
+                        SIGN UP
+                    </ButtonInline>
+                </Fragment>
+            </Form>
+        </Fragment> 
     );
 }
--- src/components/screens/Auth/components/RegisterComponent/index.tsx
+++ src/components/screens/Auth/components/RegisterComponent/index.tsx
@@ -1,11 +1,46 @@
 
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../../../utils/CONSTANTS";
+import Button from "../Button";
+import ButtonInline from "../ButtonInline";
 import Form from "../Form";
+import Input from "../Input";
+import TopContent from "../TopContent";
 
 
 export default function RegisterComponent() {
+    const navigate = useNavigate();
+    const { LOGIN, AUTH } = ROUTES;
+
+    function redirectUser(route: string) {
+        navigate(`/${AUTH}/${route}`);
+    }
     return (
-        <Form>
-            Register
-        </Form>
+        <Fragment>
+            <TopContent>
+                SIGN UP
+            </TopContent>
+
+            <Form>
+                <Fragment>
+                    <Input type="email" placeholder="put your email..." title="Email" />
+
+                    <Input type="password" placeholder="put your password..." title="Password" />
+
+                    <Button>
+                        SIGN UP
+                    </Button>
+
+                    <ButtonInline
+                        text="Already a user?"
+                        center={true}
+                        action={() => redirectUser(LOGIN)}
+                    >
+                        LOGIN
+                    </ButtonInline>
+                </Fragment>
+            </Form>
+        </Fragment> 
     );
 }
