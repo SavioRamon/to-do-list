0768af4cd08e0576cd9b509e4b675e40da658262
savioramon <savioramon12@gmail.com>
2023-03-09 19:39:33 -0300

    change theme colors

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,18 +1,21 @@
 
 export type ThemeTypes = {
     backgroundColor: string;
+    backgroundSecondary: string;
     textColor: string;
     borderColor: string;
 }
 
 export const darkTheme: ThemeTypes = {
     backgroundColor: "#1E1E1E",
+    backgroundSecondary: "#333333",
     textColor: "#FFFFFF",
     borderColor: "#A4A4A4"
 }
 
 export const lightTheme: ThemeTypes = {
-    backgroundColor: "#DFDFDF",
+    backgroundColor: "##F5F5F5",
+    backgroundSecondary: "#DFDFDF",
     textColor: "#1E1E1E",
     borderColor: "#353535"
 }
