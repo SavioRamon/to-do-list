a35c3fd7db520b36e452d02ae05335c1d222e229
savioramon <savioramon12@gmail.com>
2023-03-10 22:06:41 -0300

    creating event to login button

--- src/components/common/Header/components/LoginButton/index.tsx
+++ src/components/common/Header/components/LoginButton/index.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../../../../utils/CONSTANTS";
 import { Button } from "./style";
 
 export default function LoginButton() {
+
+    const navigate = useNavigate();
+    const { AUTH, LOGIN } = ROUTES;
+
+    function navigateToLoginScreen() {
+        navigate(`${AUTH}/${LOGIN}`);
+    }
+
     return (
-        <Button>
+        <Button onClick={navigateToLoginScreen}>
             Login
         </Button>
     )
